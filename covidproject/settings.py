00038dfb6171f67417164deb9a40c9bf6a48ed6d
Govind0123 <85865763+Govind0123@users.noreply.github.com>
2021-08-11 16:37:25 +0530

    Update settings.py
    Added REST_FRAMEWORK Auth Classes

--- covidproject/settings.py
+++ covidproject/settings.py
@@ -166,3 +166,13 @@ MEDIA_ROOT = os.path.join(PROJECT_DIR, 'media')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+}
+
+
+
