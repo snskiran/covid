9b31787b506bcfaf5f5b66a2ad763ce2aa00ce6b
Govind0123 <85865763+Govind0123@users.noreply.github.com>
2021-08-24 17:27:28 +0530

    Updated
    Updated

--- app/urls.py
+++ app/urls.py
@@ -128,7 +128,7 @@ urlpatterns = [
     ####################     SUU LABS     ####################
     path('get_taluk/', Posttaluk.as_view()),
     path('post_lab/', Postlabs.as_view()),
-    path('edit-lab/', GetEditLabs.as_view()),
+    path('get-edit-lab/', GetEditLabs.as_view()),
     path('edit-update-lab/', EditUpdateLabs.as_view()),
     path('get_all_lab/', get_all_labs.as_view()),
     path('lab_delete/', Postdelete.as_view()),
