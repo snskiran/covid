ea3ea0118a738ee2d3af4ab8d40c6084d0a4e255
Govind0123 <85865763+Govind0123@users.noreply.github.com>
2021-08-25 20:57:14 +0530

    Updated GetAllPackageDetails
    Updated GetAllPackageDetails

--- app/views.py
+++ app/views.py
@@ -3652,8 +3652,8 @@ class GetAllPackageDetails(APIView):
                 # for i in check_all_slab_collector:
                 package_details = Package_Sampling.objects.filter(Q(user_id__in=list(check_all_slab_collector))).filter((Q(package_type_status= 6) & Q(package_type_action=16)) | (Q(package_type_status=7) & Q(package_type_action=17))).values()
                 print("AAAAAA     ", package_details)
-                package_details = Package_Sampling.objects.filter(Q(user_id__in=list(check_all_slab_collector)) & ((Q(package_type_status= 6) & Q(package_type_action=16)) | (Q(package_type_status=7) & Q(package_type_action=17)))).values()
-                print("AAAAAA     ", package_details)
+#                 package_details = Package_Sampling.objects.filter(Q(user_id__in=list(check_all_slab_collector)) & ((Q(package_type_status= 6) & Q(package_type_action=16)) | (Q(package_type_status=7) & Q(package_type_action=17)))).values()
+#                 print("AAAAAA     ", package_details)
                 
                 # for pd in package_details:
                 #     check_lab = Testing_Lab_Facility.objects.filter(id=  pd['test_lab_id'])
