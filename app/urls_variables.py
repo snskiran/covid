c98f44d1767d294bb71bd1bbbd9f0f2cdf75689e
Govind0123 <85865763+Govind0123@users.noreply.github.com>
2021-09-16 00:44:59 +0530

    Updated
    Updated

--- app/urls_variables.py
+++ app/urls_variables.py
@@ -1,7 +1,7 @@
 auth_token_url = 'https://www.covidwar.karnataka.gov.in/service19_test/token'
 auth_token_grant_type = 'password'
 auth_token_username = 'uan387r8hdo9734eb83d03ec5ba1e3252d08d9ad102b8f0b2cs678sfui8udf8u'
-auth_token_username = 'vnm488d76go9734eb83d03ec5ba1e3252d08d9ad102b8f0b2cs6785562sed6vv'
+auth_token_password = 'vnm488d76go9734eb83d03ec5ba1e3252d08d9ad102b8f0b2cs6785562sed6vv'
 
 
 contact_testing_date_wise_dump_url = 'https://www.covidwar.karnataka.gov.in/service19_test/api/Values/FnSwab_GetContactTesting_Datewise'
