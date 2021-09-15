3a12b5d94950a651e5b55e8095fe928f1d284a30
Govind0123 <85865763+Govind0123@users.noreply.github.com>
2021-09-16 00:37:56 +0530

    Updated urls_variables
    Updated urls_variables

--- app/urls_variables.py
+++ app/urls_variables.py
@@ -1,4 +1,23 @@
-auth_token_username = 'Username Token'
-auth_token_username = 'Password Token'
+auth_token_url = 'https://www.covidwar.karnataka.gov.in/service19_test/token'
+auth_token_grant_type = 'password'
+auth_token_username = 'uan387r8hdo9734eb83d03ec5ba1e3252d08d9ad102b8f0b2cs678sfui8udf8u'
+auth_token_username = 'vnm488d76go9734eb83d03ec5ba1e3252d08d9ad102b8f0b2cs6785562sed6vv'
 
-token_url = 'www.token.com'
+
+contact_testing_date_wise_dump_url = 'https://www.covidwar.karnataka.gov.in/service19_test/api/Values/FnSwab_GetContactTesting_Datewise'
+contact_testing_date_wise_urban_dump_url = 'https://www.covidwar.karnataka.gov.in/service19_test/api/Values/FnSwab_GetContactTesting_Datewise_Urban'
+contact_testing_date_wise_urban_dump_url = 'https://www.covidwar.karnataka.gov.in/service19_test/api/Values/FnSwab_GetContactTesting_Datewise_Rural/'
+
+
+already_tested_patient_line_list_url = 'https://www.covidwar.karnataka.gov.in/service19_test/api/Values/FnSwab_GetAlreadyTestedData/'
+contact_testing_line_list_url = 'https://www.covidwar.karnataka.gov.in/service19_test/api/Values/FnSwab_GetContactTesting/'
+
+
+test_result_sms_url = 'https://www.covidwar.karnataka.gov.in/service19_test/api/Values/FnSwab_InsertTestResultForSMS'
+positive_test_result_line_list_url = 'https://www.covidwar.karnataka.gov.in/service19_test/api/Values/FnSwab_InsertPositiveTestResult'
+
+
+icmr_auth_token_username = 'Test'
+icmr_auth_token_password = 'testestuser123'
+icmr_access_token_url = 'https://cvstatus.icmr.gov.in/api/nic/icmr_api/index.php/login'
+icmr_add_record_url = 'https://cvstatus.icmr.gov.in/api/nic/icmr_api/index.php/add-record'
